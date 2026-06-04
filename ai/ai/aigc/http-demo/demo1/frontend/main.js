4cafa0bd2bd992fd75c50dba340304857648b401
JieE-212 <2103917668@qq.com>
2026-06-04 23:24:06 +0800

    修改

--- ai/ai/aigc/http-demo/demo1/frontend/main.js
+++ ai/ai/aigc/http-demo/demo1/frontend/main.js
@@ -1,41 +1,31 @@
-let friend = []
+let friends = [];
 
-async function loadDate() {
-  // console.log('loadDate');
-  // endpoint
-  const endpoint = 'http://localhost:3000/friend'
-  // 异步变同步
-  const res = await fetch(endpoint)
-  const data = await res.json()
-  return data
-  await fetch(endpoint)
-    // 等待响应返回
-    // 响应体是json二进制字符串 转换为json对象
-    .then(res => res.json())
-    // 处理json对象
-    .then(data => {
-      console.log(data);
-    })
+async function loadData() {
+  // console.log('loadData');
+  // endpoint 
+  const endpoint = 'http://localhost:3000/friend';
+  const res = await fetch(endpoint) // 发送请求 异步
+  // 等待响应返回
+  // 响应体是json二进制字符串 转换为json对象
+  const data = await res.json();
+  return data;
 }
 function renderData(friends) {
-  console.log('renderData');
-  const oBody = document.querySelector('table body')
-  if (friends.length > 0) {
-    oBody.innerHTML = friends.map(function(friend) {
-      console.log(friend);
-      return`<tr>
-        <td>${friend.id}</td>
-        <td>${friend.name}</td>
-        <td>${friend.age}</td>
-      </tr>`
-    }).join('')
-  }
+  const oBody = document.querySelector('table tbody');
+  oBody.innerHTML = '';
+  oBody.innerHTML = friends.length > 0 ? friends.map(friend => `
+  <tr>
+    <td>${friend.id}</td>
+    <td>${friend.name}</td>
+    <td>${friend.age}</td>
+  </tr>
+  `).join('') : '<tr><td colspan="3">暂无数据</td></tr>';
 }
 async function init() {
   console.log('init start');
-  const friends = await loadDate()
+  friends = await loadData();
   console.log(friends);
   renderData(friends);
 }
 init();
-// console.log('init end');
+// console.log('init end')
